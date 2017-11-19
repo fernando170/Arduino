024ca7f7a7ab3c8ff54a1597d923e3d8dd37e0a0
fernando170 <fer-170@live.com.mx>
2017-11-19 02:00:40 -0600

    Add files via upload

--- Proyecto_POO.ino
+++ Proyecto_POO.ino
@@ -3,6 +3,8 @@
 #include<Servo.h>
 void imprimePinLog(int s, int d);
 void imprimeUsLog(String U);
+void BeepA();
+void BeepN();
 
 class Usuario {
   private:
@@ -49,19 +51,20 @@ class Usuario {
     }
 };
 /*
-FILE *registro,*Lista;
-*registro = fopen("Registro.txt", "rw");
-*Lista = fopen("Usuarios.txt", "rw");
+  FILE *registro,*Lista;
+  registro = fopen("Registro.txt", "rw");
+  Lista = fopen("Usuarios.txt", "rw");
 */
 
-Usuario U1;
+Usuario U[3];
+int dis,claveIng;
 void setup() {
   Serial.begin(9600);
-  
+  pinMode(7, OUTPUT);
   /*String CadAux;
-  int i, temp;
-  int x=0;
-  for (i = 0; i < sizeof(Lista); i++) {
+    int i, temp;
+    int x=0;
+    for (i = 0; i < sizeof(Lista); i++) {
     if (Lista[i] == "," || Lista[i] == "\n") {
       cadAux.concat((Lista[x:i]));
       x = i + 1;
@@ -71,21 +74,40 @@ void setup() {
       Serial.print(cadAux);
       return CadAux;
     }
-  }
-  //Usuario*/
+    }
+    //Usuario*/
 }
 
 void loop() {
-  U1.setNombre("Juan Carlos");
-  U1.setAp("Bodoque");
-  U1.setAm("y Vodoque");
-  U1.setClave(1234);
-  String UsEnt;
-  UsEnt.append(U1.getNombre());
-  UsEnt.append(U1.getAp());
-  UsEnt.append(U1.getAm());
-  imprimeUsLog(UsEnt);
-  Serial.print(U1.getNombre());
+  U[0].setNombre("Ricardo");
+  U[0].setAp("Martínez");
+  U[0].setAm("Moreno");
+  U[0].setClave(1234);
+
+  U[1].setNombre("Juan Carlos");
+  U[1].setAp("Bodoque");
+  U[1].setAm("y Vodoque");
+  U[1].setClave(5678);
+
+  U[2].setNombre("Ivonne");
+  U[2].setAp("Fleurette");
+  U[2].setAm("Ewart");
+  U[2].setClave(2653);
+
+  //EL SERVO TIENE QUE ESTAR CERRADO CUANDO SE INICIE EL PROGRAMA
+
+  if (dis <= 30) {
+    for (int i = 0; i < 3; i++) {
+      if (claveIng == U[i].getClave()) {
+        EntraUs(U[i]);
+        BeepA();
+        //APERTURA DEL SERVO
+      } else {
+        BeepN();
+      }
+    }
+  }
+
 
 }
 
@@ -98,9 +120,38 @@ void imprimePinLog(String s, int d) {
 
 }
 
+void BeepA() { //Emite dos pitidos cortos en el buzzer en caso de que se otorgue la entrada
+
+  digitalWrite(9, HIGH);
+  delay(500);
+  digitalWrite(9, LOW);
+  delay(500);
+  digitalWrite(9, HIGH);
+  delay(500);
+  digitalWrite(9, LOW);
+  //delay(500);
+
+}
+
+void BeepN() { //Emite un pitido largo en el buzzer en caso de no otorrrrgar la entrada
+  digitalWrite(9, HIGH);
+  delay(1000);
+  digitalWrite(9, LOW);
+}
+
+void EntraUs(Usuario U) {
+  String UsEnt;
+  UsEnt += String(U.getNombre());
+  UsEnt += String(U.getAp());
+  UsEnt += String(U.getAm());
+  imprimeUsLog(String(UsEnt));
+  Serial.print(U.getNombre());
+}
+
 void imprimeUsLog(String U) {
   Serial.print("#S|LOG|[El usuario:");
   Serial.print(U);
   Serial.println(" ha ingresado]#");
 
 }
+
